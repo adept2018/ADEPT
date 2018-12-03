c64802c8a1bb5fc0f30d9e6d566b7e490d6c15b2
Staffan Sjoqvist <stafan87@gmail.com>
2018-12-03 19:14:35 +0100

    Fix fake laser scan package name etc.
    Signed-off-by: Staffan Sjoqvist <stafan87@gmail.com>
    
    Former-commit-id: 8f7da05e1a46a8570dc9c93d366b1b65db4870fa

--- src/fakeLaserScan/src/fakeLaserScan.cpp
+++ src/fakeLaserScan/src/fakeLaserScan.cpp
@@ -1,6 +1,8 @@
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 
+static const double angle_min= -3.14*0.9, angle_max=3.14*0.9;
+
 int main(int argc, char** argv){
   ros::init(argc, argv, "fake_laser_scan_publisher");
 
@@ -8,28 +10,28 @@ int main(int argc, char** argv){
   ros::Publisher scan_pub = n.advertise<sensor_msgs::LaserScan>("scan", 50);
 
   unsigned int num_readings = 100;
-  double laser_frequency = 40;
+  double laser_frequency = 1000;
   double ranges[num_readings];
   double intensities[num_readings];
 
   int count = 0;
-  ros::Rate r(1.0);
+  ros::Rate r(5.0);
   while(n.ok()){
     //generate some fake data for our laser scan
     for(unsigned int i = 0; i < num_readings; ++i){
       ranges[i] = 1.0 + (count % 5)/10.0;
-      intensities[i] = 100 + count; 
+      intensities[i] = 100 + count % 10; 
     }
     ros::Time scan_time = ros::Time::now();
 
     //populate the LaserScan message
     sensor_msgs::LaserScan scan;
     scan.header.stamp = scan_time;
-    scan.header.frame_id = "laser_frame";
-    scan.angle_min = -1.57;
-    scan.angle_max = 1.57;
-    scan.angle_increment = 3.14 / num_readings;
-    scan.time_increment = (1 / laser_frequency) / (num_readings);
+    scan.header.frame_id = "laser";
+    scan.angle_min = angle_min;
+    scan.angle_max = angle_max;
+    scan.angle_increment = (angle_max - angle_min) / (num_readings-1);
+    scan.time_increment = (1 / laser_frequency) / (num_readings-1);
     scan.range_min = 0.0;
     scan.range_max = 100.0;
 
